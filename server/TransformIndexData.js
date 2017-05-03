2fb1be88b9b0e758fb60d9f19dc3705537ce8bc3
Eddie Kennedy <eddiekennedy@gmail.com>
2017-05-03 17:28:55 -0400

    adding fb and tw referral data

--- server/TransformIndexData.js
+++ server/TransformIndexData.js
@@ -10,11 +10,26 @@ module.exports = class TransformIndexData {
     for ( var i = 0; i < this.data.length; i++ ) {
 
       this.data[i].chartbeat = null;
+      this.data[i].refs = { fb: 0, tw: 0 };
 
       for ( var j = 0; j < this.chartbeatData.length; j++ ) {
 
         if ( this.chartbeatData[j].path === this.data[i].path ) {
           this.data[i].chartbeat = this.chartbeatData[j].stats;
+          var toprefs = this.chartbeatData[j].stats.toprefs;
+
+          for ( var k = 0; k < toprefs.length; k++ ) {
+
+            if ( toprefs[k].domain === 'facebook.com' || toprefs[k].domain === 'm.facebook.com' ) {
+              this.data[i].refs.fb  += toprefs[k].visitors;
+            }
+
+            if ( toprefs[k].domain === 't.co' ) {
+              this.data[i].refs.tw += toprefs[k].visitors;
+            }
+
+          }
+
         }
 
       }
