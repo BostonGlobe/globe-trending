5966950b58587533bcd94672defd6deb520beeaf
Eddie Kennedy <eddiekennedy@gmail.com>
2017-05-03 18:47:23 -0400

    showing popular on message

--- server/TransformIndexData.js
+++ server/TransformIndexData.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 module.exports = class TransformIndexData {
   
   constructor(data, chartbeatData) {
@@ -7,33 +9,61 @@ module.exports = class TransformIndexData {
 
   transform() {
 
+    // loop over bg formatted chartbeat data
     for ( var i = 0; i < this.data.length; i++ ) {
 
       this.data[i].chartbeat = null;
-      this.data[i].refs = { fb: 0, tw: 0 };
+      this.data[i].refs = { fb: 0, tw: 0, g: 0 };
 
+      this.data[i].hotRef = null;
+
+      // loop over raw chartbeat data
       for ( var j = 0; j < this.chartbeatData.length; j++ ) {
 
+        // if formatted path matches raw path, do some stuff
         if ( this.chartbeatData[j].path === this.data[i].path ) {
+
           this.data[i].chartbeat = this.chartbeatData[j].stats;
           var toprefs = this.chartbeatData[j].stats.toprefs;
 
+          // loop over the top referrers
           for ( var k = 0; k < toprefs.length; k++ ) {
 
+            // facebook
             if ( toprefs[k].domain === 'facebook.com' || toprefs[k].domain === 'm.facebook.com' ) {
               this.data[i].refs.fb  += toprefs[k].visitors;
             }
 
+            // twitter
             if ( toprefs[k].domain === 't.co' ) {
               this.data[i].refs.tw += toprefs[k].visitors;
             }
 
+            // google
+            if ( toprefs[k].domain === 'google.com' ) {
+              this.data[i].refs.g += toprefs[k].visitors;
+            }
+
+          }
+
+          // set the hot referrer 
+          if ( this.data[i].refs.fb > 20 ) {
+            this.data[i].hotRef = 'on Facebook';
+          }
+
+          if ( this.data[i].refs.tw > 20 ) {
+            this.data[i].hotRef = 'on Twitter';
+          }
+
+          if ( this.data[i].refs.tw > 20 ) {
+            this.data[i].hotRef = 'Google search';
           }
 
         }
 
       }
 
+      // re-format some of the pre-formatted data
       this.data[i].image = this.data[i].image.replace('//www.bostonglobe.com/', '//c.o0bg.com/');
       this.data[i].path = this.data[i].path.replace('bostonglobe.com', '/article');
       this.data[i].path = this.data[i].path.replace('.html', '');
